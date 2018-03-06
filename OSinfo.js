f3af2beb4a3ff1e82a44238a8fc7e7dedea2ca0e
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-03-06 16:19:41 +0100

    Prettify scripts

--- OSinfo.js
+++ OSinfo.js
@@ -10,11 +10,9 @@ function getOSinfo() {
 	console.log('System: ', type);
 	console.log('Release: ', release);
 	console.log('CPU model: ', cpu);
-	console.log(`Uptime: `, upTime.print());
+	console.log('Uptime: ', upTime.print());
 	console.log('User name: ', userInfo.username);
 	console.log('Home dir: ', userInfo.homedir);
 }
 
-exports.print = getOSinfo
-
-
+exports.print = getOSinfo;
--- program.js
+++ program.js
@@ -1,24 +1,24 @@
 const OSinfo = require('./OSinfo');
 
 process.stdin.setEncoding('utf-8');
-process.stdin.on('readable', function () {
+process.stdin.on('readable', function() {
 	const input = process.stdin.read();
 	if (input !== null) {
 		const instruction = input.toString().trim();
-		switch(instruction) {
-		 	case '/exit':
+		switch (instruction) {
+			case '/exit':
 				process.stdout.write('Quitting app!\n');
 				process.exit();
-		 		break;
-		 	case '/info' :
+				break;
+			case '/info':
 				// process.stdout.write(`\nLanguage: ${process.env.LANGUAGE}\nNode version: ${process.versions.node}\n`);
-			 	console.log(`Language: ${process.env.LANGUAGE}`);
-			 	console.log(`Node version: ${process.versions.node}`);
-			 	break;
-			case '/getOSinfo' :
+				console.log(`Language: ${process.env.LANGUAGE}`);
+				console.log(`Node version: ${process.versions.node}`);
+				break;
+			case '/getOSinfo':
 				OSinfo.print();
 				break;
-		 	default:
+			default:
 				process.stderr.write('Wrong instruction\n');
 		}
 	}
